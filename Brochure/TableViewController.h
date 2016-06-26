8cfc204fa9ca3c95874a8fc1bd588454ae443bcf
Akshat <omakshatmittal@gmail.com>
2016-06-27 02:19:35 +0530

    Setting the tab bar and it's item.

---
+++ Brochure/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Brochure
+//
+//  Created by Akshat Mittal on 27/06/16.
+//  Copyright © 2016 Akshat Mittal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UIViewController
+
+@end
